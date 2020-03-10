e16a972d72707d98b1a9d513cb87eb6ae2c6351a
dot <shuhei.kondo@gmail.com>
2020-03-10 18:20:35 +0900

    init

---
+++ Brewfile
@@ -0,0 +1,139 @@
+tap "github/gh"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/core"
+tap "homebrew/services"
+tap "neovim/neovim"
+tap "puma/puma"
+brew "readline"
+brew "sqlite"
+brew "xz"
+brew "awscli"
+brew "bat"
+brew "colordiff"
+brew "coreutils"
+brew "curl"
+brew "direnv"
+brew "docker"
+brew "docker-machine"
+brew "elixir"
+brew "exa"
+brew "fd"
+brew "fish"
+brew "fzf"
+brew "gettext"
+brew "ghq"
+brew "git"
+brew "git-secrets"
+brew "python"
+brew "glib"
+brew "pinentry"
+brew "gnupg"
+brew "go"
+brew "hexyl"
+brew "highlight"
+brew "hub"
+brew "jq"
+brew "lv"
+brew "mas"
+brew "mosh"
+brew "neovim"
+brew "nkf"
+brew "pkg-config"
+brew "node-build"
+brew "nodenv"
+brew "packer"
+brew "peco"
+brew "pinentry-mac"
+brew "postgresql", restart_service: true
+brew "procs"
+brew "pulumi"
+brew "ruby-build"
+brew "rbenv"
+brew "rbenv-default-gems"
+brew "rbenv-gemset"
+brew "reattach-to-user-namespace"
+brew "renameutils"
+brew "ripgrep"
+brew "sd"
+brew "shellcheck"
+brew "ssh-copy-id"
+brew "starship"
+brew "the_platinum_searcher"
+brew "the_silver_searcher"
+brew "tig"
+brew "tmux"
+brew "tree"
+brew "vim"
+brew "wget"
+brew "github/gh/gh"
+brew "puma/puma/puma-dev"
+brew 'mackup'
+cask "1password"
+cask "alacritty"
+cask "alfred"
+cask "appcleaner"
+cask "appzapper"
+cask "atom"
+cask "bettertouchtool"
+cask "betterzip"
+cask "box-drive"
+cask "brave-browser"
+cask "choosy"
+cask "cyberduck"
+cask "dash"
+cask "firefox"
+cask "font-fira-code"
+cask "font-hackgen"
+cask "font-noto-sans-cjk-jp"
+cask "font-noto-serif-cjk-jp"
+cask "font-ricty-diminished"
+cask "font-roboto"
+cask "font-source-code-pro"
+cask "font-source-han-code-jp"
+cask "font-source-han-sans"
+cask "github"
+cask "google-chat"
+cask "google-chrome"
+cask "hyperswitch"
+cask "libreoffice"
+cask "onyx"
+cask "qlmarkdown"
+cask "qlstephen"
+cask "quicklook-json"
+cask "spectacle"
+cask "synergy"
+cask "tableplus"
+cask "virtualbox"
+cask "visual-studio-code"
+cask "vivaldi"
+cask "vlc"
+cask "wkhtmltopdf"
+cask "xquartz"
+cask 'karabiner-elemenets'
+mas "Amphetamine", id: 937984704
+mas "BetterSnapTool", id: 417375580
+mas "CotEditor", id: 1024640650
+mas "DaisyDisk", id: 411643860
+mas "EdgeView 2", id: 1206246482
+mas "Gyazo", id: 490085255
+mas "Hex Fiend", id: 1342896380
+mas "Kaleidoscope", id: 587512244
+mas "Keynote", id: 409183694
+mas "Kindle", id: 405399194
+mas "LINE", id: 539883307
+mas "Noizio", id: 928871589
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Pixelmator", id: 407963104
+mas "Pocket", id: 568494494
+mas "Pushbullet", id: 948415170
+mas "Quiver", id: 866773894
+mas "Remote Desktop", id: 409907375
+mas "Slack", id: 803453959
+mas "StuffIt Expander", id: 919269455
+mas "TweetDeck", id: 485812721
+mas "Twitter", id: 1482454543
+mas "WiFi Explorer", id: 494803304
+mas "Xcode", id: 497799835
---
+++ setup.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+# Command Line Tools for Xcode
+xcode-select --install
+
+# clone repo
+WORKDIR=~/projects/github.com/dot/
+mkdir $WORKDIR
+git clone https://github.com/dot/kitting.git $WORKDIR
+cd $WORKDIR
+
+# setup HomeBrew
+if type brew >/dev/null 2>&1; then
+  echo "start install and bundle brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  brew upgrade
+  brew bundle
+fi
+
+# setup VSCode
+if type code >/dev/null 2>&1; then
+  echo 'Setup VSCode'
+  code --install-extension Shan.code-settings-sync -force
+  echo "done"
+fi
+
+# restore via mackfile
+if [ ! -e "~/.mackup.cfg" ]; then
+  echo 'Setup Mackup'
+  cat <<EOF > ~/.mackup.cfg
+[storage]
+engine = icloud
+EOF
+  cd $HOME
+  mackup restore -f
+fi
+
+
+# setup OSX settings
+sudo -v
+killall System\ Preferences
+
+# 起動音を消す
+sudo nvram SystemAudioVolume=" "
+
+# keyrepeat
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g InitialKeyRepeat -int 15 # 150ms
+defaults write -g KeyRepeat -int 2 # 15ms
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable auto capitalize
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# Disable auto period insert
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+# Disable 'smart' quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# Disable smart dashes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# ネットワークボリュームに DS_Storeを作らない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# ライブラリフォルダを表示
+sudo chflags nohidden ~/Library
+sudo chflags nohidden /opt
+sudo chflags nohidden /Volumes
+
+# date and clock
+defaults write com.apple.menuextra.clock 'DateFormat' -string 'EEE H:mm:ss'
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool true
+
+# setup Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+# Don't show recents
+defaults write com.apple.dock show-recents -bool false
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# ファンクションキーの設定
+# see http://r7kamura.github.io/2014/08/03/as-standard-function-keys.html
+defaults write -g com.apple.keyboard.fnState -bool true
+
+# USB やネットワークストレージに .DS_Store ファイルを作成しない
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# for Safari
+
+# tremimnal
+defaults write com.apple.Terminal "Default Window Settings" -string "Homebrew"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Homebrew"
+
+# touch pad behaviour
+defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -int 1
+
+#defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+# kill hardware chime for macbook when battery powered
+defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
+
+# restart
+killall Finder
+killall Dock
+killall PowerChime
